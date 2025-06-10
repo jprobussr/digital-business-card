63fc074b4063e28e9bbc544e7400ff05eb57cab2
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-06-10 16:24:43 -0500

    Update Styles and add a light/dark mode

--- DigitalBusinessCard/src/App.jsx
+++ DigitalBusinessCard/src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CardHeader from './components/CardHeader.jsx';
 import CardBody from './components/CardBody.jsx';
 import CardFooter from './components/CardFooter.jsx';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
-    <div className='app'>
-     <CardHeader />
-     <CardBody />
-     <CardFooter />
+    <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
+      <div className="card">
+        <div className="toggle-container">
+          <button
+            className="theme-toggle"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+            aria-label='Toggle theme'
+          >
+           <i className={`fa-solid ${isDarkMode ? 'fa-sun' : 'fa-moon'}`}></i>
+          </button>
+        </div>
+        <CardHeader />
+        <CardBody />
+        <CardFooter />
+      </div>
     </div>
   );
 };
